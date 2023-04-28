80ef613af6da555e128953c2885caa9b6857923c
Shivam1456 <127660326+Shivam1456@users.noreply.github.com>
2023-04-28 07:54:38 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your code here
+
+// <!-- Include the Bootstrap and jQuery libraries -->
+// <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+// <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+// <script src="http
+// s://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+// <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
